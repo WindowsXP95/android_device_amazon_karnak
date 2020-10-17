23ca0965115bac9c6244a87f46622a2c8df717d1
Jaiden Nowak <windowsxp519@gmail.com>
2020-10-17 03:49:46 -0400

    renamed device name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -16,7 +16,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Amazon Fire"
+#define BTM_DEF_LOCAL_NAME   "Amazon Fire HD8"
 #define BTA_AV_MAX_A2DP_MTU  1788
 #define BLE_INCLUDED TRUE
 #define BTA_GATT_INCLUDED TRUE
